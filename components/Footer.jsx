5561dd34e90e1bd8d3dd10fb608a11113ab34acf
gurungbsal18 <gurungbsal18@gmail.com>
2025-03-24 19:00:36 +0545

    year added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { FaHeart } from "react-icons/fa";
 
 function Footer() {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
   return (
     <div>
-      <p className="text-white text-sm flex justify-center items-center gap-2 py-4">
+      <p className="text-white text-sm text-center flex justify-center items-center gap-2 py-4">
         <FaHeart className="text-red-600" /> Build By Bishal Gurung
+        <br />
+        &copy; {currentYear}
       </p>
     </div>
   );
